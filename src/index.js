13c1febe1c1b2faaa36ccd2b9f185bde0e8d5566
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-09-17 22:15:26 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let sequence = {};
+  for (let i in bracketsConfig) {
+    sequence[bracketsConfig[i][1]] = bracketsConfig[i].join("");
+  }
+  let copyStr = str;
+  for (let i = 0; i < str.length; i++) {
+    copyStr = copyStr.replace(sequence[str[i]], "");
+  }
+  if (copyStr.length !== 0) {
+    return false;
+  }
+  else{
+    return true;
+  }
 }
